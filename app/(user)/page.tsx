7aabb8d8425c186ee4251fd231b6cfbb9a212416
parker cheng <fbi0258zzz@gmail.com>
2023-01-27 11:57:33 +0800

    init post type

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,6 +1,25 @@
 import { previewData } from 'next/headers';
+import { groq } from 'next-sanity';
+import { client } from '../../lib/sanity.client';
+import { PreviewSuspense } from 'next-sanity/preview';
+import BlogList from '../../components/BlogList';
+import PreviewBlogList from '../../components/PreviewBlogList';
 
-export default function HomePage() {
-  if (previewData()) return <div>Preview mode</div>;
-  return <div>Not in preview mode</div>;
+const query = groq`
+*[_type=='post']{
+...,
+author->,
+categories[]->
+}|order(_createAt desc)`;
+
+export default async function HomePage() {
+  if (previewData())
+    return (
+      <PreviewSuspense fallback="Loading">
+        <PreviewBlogList query={query} />
+      </PreviewSuspense>
+    );
+  const posts = await client.fetch(query);
+  console.log(posts);
+  return <BlogList posts={posts} />;
 }
