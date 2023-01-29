0620c3f7278ff9d12cbbc85516fdca629803c373
EustaceCheng <fbi0258zzz@gmail.com>
2023-01-29 23:34:14 +0800

    Update BlogList.tsx

--- components/BlogList.tsx
+++ components/BlogList.tsx
@@ -1,5 +1,5 @@
-import Image from 'next/image';
-import urlFor from '../lib/urlFor';
+import Image from "next/image";
+import urlFor from "../lib/urlFor";
 
 type Props = {
   posts: Post[];
@@ -12,11 +12,7 @@ function BlogList({ posts }: Props) {
       <div>
         {posts.map((post) => (
           <div key={post._id} className="flex flex-col group cursor-pointer">
-            <div
-              className="relative w-full h-80 
-          drop-shadow-xl group-hover:scale-105
-           transition-transform duration-200 ease-out"
-            >
+            <div className="relative w-full h-80 drop-shadow-xl group-hover:scale-105 transition-transform duration-200 ease-out  ">
               <Image
                 className=" object-cover object-left lg:object-center"
                 src={urlFor(post.mainImage).url()}
@@ -33,14 +29,28 @@ function BlogList({ posts }: Props) {
                 <div>
                   <p className="font-bold">{post.title}</p>
                   <p>
-                    {new Date(post._createdAt).toLocaleDateString('en-US', {
-                      day: 'numeric',
-                      month: 'long',
-                      year: 'numeric',
+                    {new Date(post._createdAt).toLocaleDateString("en-US", {
+                      day: "numeric",
+                      month: "long",
+                      year: "numeric",
                     })}
                   </p>
                 </div>
+                <div>
+                  {post.categories.map((category) => (
+                    <div
+                      key={category._id}
+                      className="bg-[#F7AB0A] text-center text-black px-3 py-1 rounded-full text-sm font-semibold"
+                    >
+                      <p>{category.title}</p>
+                    </div>
+                  ))}
+                </div>
               </div>
+            </div>
+            <div className="mt-5 flex-1">
+              <p className="underline text-lg font-bold">{post.title}</p>
+              <p className="line-clamp-2 text-gray-500">{post.description}</p>
             </div>
           </div>
         ))}
